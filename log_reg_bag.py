4ee5a01526353344e8095ff7271d8da597df45a9
LING ZHANG <junelena1313@gmail.com>
2018-05-04 22:46:54 -0400

    try2

--- log_reg_bag.py
+++ log_reg_bag.py
@@ -1,18 +1,13 @@
 import graphlab
 import csv
-import sys
+import operator
 import pandas as pd 
 from itertools import izip
-"""
-argv[0]: program name
-argv[1]: train file
-argv[2]: test file
-argv[3]: result file
-"""
 
-def procedure(train_test, test_data):
 
-    sents = graphlab.SFrame(sys.argv[1])
+def procedure(train_data, test_data):
+
+    sents = train_data
 
     sents['word_count'] = graphlab.text_analytics.count_words(sents['text'])
 
@@ -28,9 +23,9 @@ def procedure(train_test, test_data):
 
     test_data.head()
 
-    model = graphlab.logistic_classifier.create(sents, target='sentiment', features=['word_count'], validation_set=None)
+    model = graphlab.logistic_classifier.create(sents, target='sentiment', features=['word_count'], convergence_threshold=0.01, validation_set=None)
 
-    predictions = model.classify(test_data)
+    #predictions = model.classify(test_data)
 
     pred_class = model.predict(test_data, output_type = "class")
     
@@ -55,9 +50,10 @@ def main():
     test_data = graphlab.SFrame('clean_test2.csv')
 
     # record results: list of dicts
-    aggregate = [{0: 0, 1: 0, 2: 0, 3: 0, 4: 0}]*test_data.num_rows
+    aggregate = [{0: 0, 1: 0, 2: 0, 3: 0, 4: 0}]*test_data.num_rows()
+    print aggregate[0]
 
-    for idx in range():
+    for idx in range(1):
         
         data = pd.read_csv('clean_train2.csv', sep=',', header=0, engine='python')
         sample_data = data.sample(frac=1, replace=True)
@@ -70,7 +66,8 @@ def main():
     
 
     ids = list(range(1455))
-    with open('bagg_pred', 'wb') as f:
+    with open('bagg_pred.csv', 'wb') as f:
         writer = csv.writer(f)
         writer.writerows(izip(ids, final_results))
 
+if __name__ == "__main__": main()
